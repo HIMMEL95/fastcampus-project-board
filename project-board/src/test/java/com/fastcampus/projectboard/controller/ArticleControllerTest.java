a091d10a42c76f1c52cf6479ef3ce14d31a8d323
HIMMEL <bbluesky7738@gmail.com>
2022-07-31 07:26:18 +0900

    불필요한 import 삭제

--- project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -3,7 +3,6 @@ package com.fastcampus.projectboard.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcBuilderCustomizer;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
